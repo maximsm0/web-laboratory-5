dfd7045bd9784acb0a79f0f6c01edbc550cbc9d8
maxims <maxstorozhuk4@gmail.com>
2025-05-10 18:53:00 +0300

    Добавил ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y_v(1_rjz4d&r=hu*d&a^ekl07!-zu7xh=5qu@)8+z6bqvgrj)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
