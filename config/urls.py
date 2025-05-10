a2226297c97dfa0e3d652c121dc80f3d26ad3065
maxims <maxstorozhuk4@gmail.com>
2025-05-10 19:10:08 +0300

    Добавлена копия приложения products_copy и подключена к проекту

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
+    path('copy/', include('products_copy.urls')),
 ]
